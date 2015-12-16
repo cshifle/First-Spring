c041972d2a20a2fa1ae4e9ebd1dc1b14bee27fa3
cshifle <cshiffle1991@gmail.com>
2015-12-16 17:25:28 -0500

    add initial files

---
+++ Java Programs/spring-hello-world/src/main/java/shifflett/chris/spring/App.java
@@ -0,0 +1,20 @@
+package shifflett.chris.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		//POJO - Plain old java object
+		
+		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		
+		
+		Person person = (Person)context.getBean("person");
+		person.speak();
+		
+		
+	}
+
+}
---
+++ Java Programs/spring-hello-world/src/main/java/shifflett/chris/spring/Person.java
@@ -0,0 +1,7 @@
+package shifflett.chris.spring;
+
+public class Person {
+	public void speak(){
+		System.out.println("Hello world, i am a person!");
+	}
+}
